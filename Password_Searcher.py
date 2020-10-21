017f8f1e121a28260879a60fafc63d5ac01ec78a
Sp33dRunn3r <68667434+Sp33dRunn3r@users.noreply.github.com>
2020-10-20 21:04:17 -0400

    Add files via upload

---
+++ Password_Searcher.py
@@ -0,0 +1,35 @@
+import requests
+import hashlib4
+import sys
+
+def request_api_data(query_char): #(Connecting to and accessing the "PWNED_PASSWORDS" website")
+    url = 'https://api.pwnedpasswords.com/range/' + query_char
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(f'Error fetching: {res.status_code}, Check the api and try again')
+    return res
+
+def get_password_leaks_count(hashes, hash_to_check): #(Categorizing and splitting hashes based on variable input)
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, count in hashes:
+        if h == hash_to_check:
+            return count
+    return 0
+
+def pwned_api_check(password): #(Utilizing the functionality of the "PWNED website")
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    first5_char, tail = sha1password[:5], sha1password[5:]
+    response = request_api_data(first5_char)
+    return get_password_leaks_count(response, tail)
+
+def main(args): #(Finishing the program with a determiner)
+    for password in args:
+        count = pwned_api_check(password)
+        if count:
+            print(f'{password} was found  {count} times..... You should definitely change your password!')
+        else:
+            print(f'{password} was NOT found. Carry on!')
+        return 'done!'
+
+if __name__ == '__main__': #(Final revertion)
+    sys.exit(main(sys.argv[1:]))
